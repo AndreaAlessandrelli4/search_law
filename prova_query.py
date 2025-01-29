4710bcbd88f1103a034d81bdf3745d490da576b8
Andrea Alessandrelli <99531185+AndreaAlessandrelli4@users.noreply.github.com>
2025-01-29 12:07:17 +0100

    Update prova_query.py

--- prova_query.py
+++ prova_query.py
@@ -171,7 +171,7 @@ for main_key, sub_filters in filters_structure.items():
             elif isinstance(options, list) and len(options) == 2 and all(isinstance(i, int) for i in options):
                 # Slider per range numerici
                 selected_filters[main_key][sub_key] = st.slider(
-                    f"{sub_key}:",
+                    f"{sub_key.split('__')[-1].replace('_',' ')}:}:",
                     min_value=options[0],
                     max_value=options[1],
                     value=(options[0], options[1]),
@@ -222,7 +222,7 @@ if st.button("Esegui Ricerca"):
                     }]}  )
             elif value != "NON SPECIFICATO":  # Applica il filtro solo se diverso da "NON SPECIFICATO"
                 weaviate_filters["operands"].append({
-                    "path": [f"{sub_key.split('__')[-1].replace('_',' ')}"],
+                    "path": [f"{sub_key}"],
                     "operator": "Equal",
                     "valueText": value
                 })
