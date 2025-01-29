ae781b74700670d92a159f47a609cd13a11a48af
Andrea Alessandrelli <99531185+AndreaAlessandrelli4@users.noreply.github.com>
2025-01-29 16:49:28 +0100

    Update prova_query.py

--- prova_query.py
+++ prova_query.py
@@ -71,7 +71,7 @@ st.markdown(
                                 { color: #007BFF !important; }
 
 
-        div.stSlider > div[data-baseweb = "slider"] > div > div {background: #007BFF !important;}
+
 
         /* Linea di separazione nera */
         hr {
