e264bf7e5b92e28f4e664e67d009fbe88af496e1
Andrea Alessandrelli <99531185+AndreaAlessandrelli4@users.noreply.github.com>
2025-01-29 01:19:40 +0100

    Update prova_query.py

--- prova_query.py
+++ prova_query.py
@@ -117,6 +117,7 @@ filters_structure = {
         "dettagli_matrimonio__regime_patrimoniale": ['comunione dei beni', 'separazione dei beni', 'NON SPECIFICATO']
     },
     'dettagli_figli': {
+        "dettagli_figli__numero_totale_figli": [0, 10],
         "dettagli_figli__numero_di_figli_minorenni": [0, 10],
         "dettagli_figli__numero_di_figli_maggiorenni_economicamente_indipendenti": [0, 10],
         "dettagli_figli__numero_di_figli_maggiorenni_non_economicamente_indipendenti": [0, 10],
@@ -207,7 +208,7 @@ if st.button("Esegui Ricerca"):
                     }]}  )
             elif value != "NON SPECIFICATO":  # Applica il filtro solo se diverso da "NON SPECIFICATO"
                 weaviate_filters["operands"].append({
-                    "path": [f"{sub_key}"],
+                    "path": [f"{sub_key.split('__')[-1].replace('_',' ')}"],
                     "operator": "Equal",
                     "valueText": value
                 })
