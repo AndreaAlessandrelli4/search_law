85d6e371db7ce8e8108a87ef2569bad2aa04d841
Andrea Alessandrelli <99531185+AndreaAlessandrelli4@users.noreply.github.com>
2025-02-05 18:57:30 +0100

    Update prova_query.py

--- prova_query.py
+++ prova_query.py
@@ -202,7 +202,7 @@ def query_weaviate(query, num_max, alpha, filters, search_prop=["testo_parziale"
             if ids_temp not in ids:
                 ids.append(ids_temp)
                 diz = {}
-                diz['query_score'] = i["_additional"]["explainScore"]
+                diz['query_score'] = score(i["_additional"]["explainScore"])
                 diz['id_originale'] = i['id_originale']
                 diz['summary'] = i['summary']
                 diz['testo_completo'] = i['testo_completo']
